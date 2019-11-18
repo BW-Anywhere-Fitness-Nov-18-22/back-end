8e0814b98b991d3a7971a5359e8165cbc580bc10
David Kuseh <davegru203@gmail.com>
2019-11-18 19:49:05 +0000

    fixed register endpoint to remove password

--- routers/authentication/authRouter.js
+++ routers/authentication/authRouter.js
@@ -6,13 +6,20 @@ const { generateToken } = require('../../helpers/tokenize');
 
 
 router.post('/register', async(req, res) => {
-    const {password} = req.body;
-    const hash = bcrypt.hashSync(password, 14);
-    req.body.password = hash;
-
     try {
-        const user = await Users.addUser(req.body);
+        const {password} = req.body;
+        const hash = bcrypt.hashSync(password, 14);
+        req.body.password = hash;
+
+        let user = await Users.addUser(req.body);
         const token = await generateToken(user);
+        user = {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            role: user.role
+            }
         res.status(201).json({message: 'Welcome!!!!', token, user })
     } catch (error) {
         res.status(500).json({errorMessage: error.message});
@@ -31,7 +38,8 @@ router.post('/login', async(req,res) => {
                 firstName: user.firstName,
                 lastName: user.lastName,
                 email: user.email,
-                role: user.role}
+                role: user.role
+                }
             res.status(201).json({message: 'Welcome!!!!', token, user })
         }
     }
