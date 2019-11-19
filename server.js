51dd160f4cf00e9261f82173906f2d794834af8e
DimejiAre <olardi2@gmail.com>
2019-11-19 14:28:56 +0100

    add restricted middleware to class router

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const authRouter = require('./routers/authentication/authRouter');
 const classRouter = require('./routers/classes/classRouter');
+const {restricted} = require('./helpers/middleware');
 
 const server = express();
 
@@ -13,7 +14,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
-server.use('/api/class', classRouter);
+server.use('/api/class', [restricted], classRouter);
 
 server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
