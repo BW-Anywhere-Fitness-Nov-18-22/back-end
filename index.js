21577dfcc7f2c275c7abb8ad724b088057890481
Josemaria Nriagu <josemarianriagu25@gmail.com>
2019-11-20 10:51:48 +0800

    adjust relative file path
    Co-authored-by: DimejiAre <olardi2@gmail.com>
    Co-authored-by: David Kuseh <davegru203@gmail.com>
    Co-authored-by: Oluwatola Oluwafemi Blessing <53811965+Oluwa-Femi@users.noreply.github.com>
    Co-authored-by: Sandrava Philips <lacasandra1@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const server = require('./server');
+const server = require('./api/server');
+
 const port = process.env.PORT || 4000
 
 server.listen(port, ()=>{
-  console.log(`listening on http://localhost:${port}`)  
+  console.log(`Server listening on ${port}`)  
 })
